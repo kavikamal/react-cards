3d1c290cfea7f1185409ab4e78b32cd2dba4f3db
Kavitha Kamal <kavithakamal@gmail.com>
2018-05-23 16:21:05 -0400

    Code cleanup

--- src/App.js
+++ src/App.js
@@ -23,9 +23,8 @@ const Card = (props) => (
             <div className="card__description cardGroup__cardDescription">
                 <div className={`icon fa ${props.icon} card__descriptionIcon`} />
                 <div className="card__descriptionText">
-                {props.hint && <span>{props.hint}</span>}
-                <br/>
                 {props.description && <span>{props.description}</span>}
+                {props.hint && <React.Fragment><br/>({props.hint})</React.Fragment>}               
                 </div>
             </div>
             <div className="card__price">{props.price}</div>
@@ -33,10 +32,11 @@ const Card = (props) => (
 );
 
 const App = () => (
+   
     <CardGroup className="cardGroup">
         <Card className="card__descriptionText" description="Trial" icon="fa-thumbs-o-up" price="Free"/>    
-        <Card className="card__descriptionText" description="Basic tier" hint="(most popular)" icon="fa-trophy" price="$10.00" />
-        <Card className="card__descriptionText" description="Advanced tier" hint="(only for enterprise-level professionals)" icon="fa-bolt" price="$6000.00" />      
+        <Card className="card__descriptionText" description="Basic tier" hint="most popular" icon="fa-trophy" price="$10.00" />
+        <Card className="card__descriptionText" description="Advanced tier" hint="only for enterprise-level professionals" icon="fa-bolt" price="$6000.00" />      
     </CardGroup>
 );
 
